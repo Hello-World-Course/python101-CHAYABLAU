50108c6b05bf3a3f7e25688276b40f99f3b6bb89
CHAYABLAU <b0533174621@gmail.com>
2025-05-20 16:18:19 +0300

    fix the mistake laster

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -4,6 +4,4 @@ board_size= int(input(f"{name}, please choose board size"))
 
 number_of_mines= int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
 
-
-
 print(f"{name}, the board size is: {board_size} ,the number of mines is: {number_of_mines}. ENJOY!")
