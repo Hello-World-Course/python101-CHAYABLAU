57ea7649fa7240f68926fe037039d831e7fbe26a
CHAYABLAU <b0533174621@gmail.com>
2025-05-21 21:57:14 +0300

    fix the logica

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,21 +1,35 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
-name =None
-board_size =None
-number_of_mines= None
-name = input("Hello, whats your name")
-if(len(name)<2):
-    print("Your name is too short")
-    name=None
 
-else:
-    board_size = int(input(f"{name}, please choose board size"))
-    if( board_size<=0 or board_size>=26):
-       print(f"{name}, you have entered illegal board size")
-       board_size=None
-       number_of_mines=None
+def is_name_valid(name):
+    if len(name) >2:
+        return True
+    return False
+def is_board_size_valid(board_size):
+    if board_size >0 and board_size < 26:
+        return True
+    return False
+def is_number_of_mines_valid( board_size,number_of_mines):
+    if number_of_mines <board_size*board_size/2:
+        return True
+    return False
+def register_user():
+    name = input("Hello, whats your name")
+    if (is_name_valid(name)==False):
+        print("Your name is too short")
+        return None,None,None
     else:
-        number_of_mines= int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
-        if(number_of_mines>(board_size * board_size) /2 or number_of_mines<=0):
-           print(f"{name}, you have entered illegal number of mines")
-           number_of_mines=None
-        else:print(f"{name}, the board size is: {board_size} ,the number of mines is: {number_of_mines}. ENJOY!")
+        board_size = int(input(f"{name}, please choose board size"))
+        if (is_board_size_valid(board_size)==False):
+            print(f"{name}, you have entered illegal board size")
+            return None, None, None
+        else:
+            number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
+            if (is_number_of_mines_valid(board_size,number_of_mines)==False):
+                print(f"{name}, you have entered illegal number of mines")
+                return None, None, None
+            else:
+                return name,board_size,number_of_mines
+
+
+user_name, user_board_size, user_num_mines = register_user()
+print(f"name:{user_name}, board size:{user_board_size}, number of mines:{user_num_mines}")
