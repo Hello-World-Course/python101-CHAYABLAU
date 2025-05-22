5e96758472749b281094f00764227750b2ab27ef
CHAYABLAU <b0533174621@gmail.com>
2025-05-22 21:10:54 +0300

    fix the logica

--- project/board/board_functions.py
+++ project/board/board_functions.py
@@ -1,4 +1,11 @@
 def is_on_board(x, y, board):
     # This is wrong fix it:
-    if len(board) > x > -1:
+    if len(board) > x and len(board) > y:
         return True
+    return False
+
+def safe_set_value( x, y, value, board):
+    if is_on_board(x, y, board):
+        board[x][y] = value
+        return True
+    return False
