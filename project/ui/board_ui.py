55ccacce67a42378d3a80ec7453b87f050df2f5d
CHAYABLAU <b0533174621@gmail.com>
2025-05-27 11:41:19 +0300

    try task

--- project/ui/board_ui.py
+++ project/ui/board_ui.py
@@ -5,6 +5,37 @@ def parse_cmd(command):
     return command_name[0], parameters
 
 
+def draw_board(board):
+    # rows = len(board)
+    size = len(board)
+    index_width = len(str(size))
+    result = " " * index_width
+    for i in range(size):
+        result = result_width + chr(ord('A') + i) + " "
+    result += "\n"
+    for row_index, row in enumerate(board):
+      row_str = row_index + " |"
+      for column in row:
+        row_str += column + "|"
+    result += row_str + "\n"
+    return result
+
+def convert_coords(location: str) -> tuple[int, int]:
+    row_str = ""
+    col_char = ""
+
+    # נפריד את הספרות מהאות (תומך גם ב-10A, 12C וכו')
+    for char in location:
+        if char.isdigit():
+            row_str += char
+        else:
+            col_char += char.upper()
+
+    row = int(row_str)
+    col = ord(col_char) - ord('A')
+
+    return row, col
+
 # This is a run example, use this to verify your code
 # you should remove this code later
 command, parameters = parse_cmd("test_command a b c d e")
